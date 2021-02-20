dafe8ee6b19defc138c05cd2ff597d4390114acf
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-02-19 18:04:16 -0800

    Update query.sql

--- query.sql
+++ query.sql
@@ -16,3 +16,21 @@ ON e.emp_no = s.emp_no;
 SELECT first_name AS "First Name", last_name AS "Last Name", hire_date AS "Hire Date"
 FROM employees
 WHERE EXTRACT(YEAR FROM hire_date) = '1986';
+
+
+-- Department managers details 
+-- (dept number, dept name, manager number, last name, first name)
+SELECT * FROM dept_manager;
+
+SELECT * FROM departments;
+
+SELECT * FROM employees LIMIT 10;
+
+SELECT dm.dept_no AS "Department Number", e.emp_no AS "Manager Number", 
+       e.last_name AS "Last Name", 
+       e.first_name AS "First Name"
+FROM employees AS e
+JOIN dept_manager AS dm
+ON e.emp_no = dm.emp_no
+LIMIT 10;
+
