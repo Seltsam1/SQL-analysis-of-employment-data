682408dc320c17e0b8eeff702db514370a0024c6
Brie Pfisterer <75340215+Seltsam1@users.noreply.github.com>
2021-02-19 17:45:25 -0800

    Updated query.sql

--- query.sql
+++ query.sql
@@ -4,17 +4,15 @@
 
 
 -- Employee details (number, last name, first name, sex, and salary)
-SELECT * FROM employees LIMIT 50;
-
-SELECT e.emp_no AS "Number", e.last_name AS "Last Name", 
-	   e.first_name AS "First Name", e.sex AS "Sex"
-FROM employees AS e
-LIMIT 20;
-
 SELECT e.emp_no AS "Number", e.last_name AS "Last Name", 
 	   e.first_name AS "First Name", e.sex AS "Sex",
 	   s.salary AS "Salary"
 FROM employees AS e
 LEFT JOIN salaries AS s
-ON e.emp_no = s.emp_no
-LIMIT 20;
+ON e.emp_no = s.emp_no;
+
+
+-- First name, last name, and hire date for employees hired in year 1986
+SELECT first_name AS "First Name", last_name AS "Last Name", hire_date AS "Hire Date"
+FROM employees
+WHERE EXTRACT(YEAR FROM hire_date) = '1986';
